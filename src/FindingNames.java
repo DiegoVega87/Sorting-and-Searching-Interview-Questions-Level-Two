efa921d5b9f3f3a9b9c91ee34d7dd7fa3d9e2ba3
Diego Vega <diegovega@me.com>
2023-08-05 18:08:55 +0200

    Add examples

--- src/FindingNames.java
+++ src/FindingNames.java
@@ -4,5 +4,17 @@ public class FindingNames {
     * We need to generate all possible variations of product names that we can use for marketing purposes. To this end,
     * our startup needs a function that takes in an array of distinct characters and returns all
     * the possible permutations of the characters.
-     * */
+    *
+    * Example 1:
+    * char[] chars = {'a', 'b', 'c'};
+    * List<String> result = generateProductNames(chars);
+    * Output: [abc, acb, bac, bca, cab, cba]
+    *
+    * Example 2:
+    * char[] chars = {'1', '2', '3', '4'};
+    * List<String> result = generateProductNames(chars);
+    * Output:[1234, 1243, 1324, 1342, 1423, 1432, 2134, 2143, 2314, 2341, 2413, 2431, 3124, 3142,
+    *  3214, 3241, 3412, 3421, 4123, 4132, 4213, 4231, 4312, 4321]
+    *
+    * */
 }
