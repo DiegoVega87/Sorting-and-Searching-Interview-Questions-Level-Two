421dc3ecab023ebb54b0e2e72f9445f2b6f437fc
Diego Vega <diegovega@me.com>
2023-08-05 18:06:40 +0200

    Add proble explanation

---
+++ src/FindingNames.java
@@ -0,0 +1,8 @@
+public class FindingNames {
+
+    /*
+    * We need to generate all possible variations of product names that we can use for marketing purposes. To this end,
+    * our startup needs a function that takes in an array of distinct characters and returns all
+    * the possible permutations of the characters.
+     * */
+}
