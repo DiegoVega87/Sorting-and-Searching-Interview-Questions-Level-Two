35e82099371087ceee6ab8ed61a8c5377b729eec
Diego Vega <diegovega@me.com>
2023-08-08 17:35:03 +0200

    Add problem explanation

---
+++ src/MostSoldProducts.java
@@ -0,0 +1,9 @@
+public class MostSoldProducts {
+
+    /*
+    * As a startup, we want to analyze our customers' purchase history to identify the top k frequently used products.
+    * We need you to write a function that takes in an integer array of sold product IDs and an integer k, and returns
+    * the k most frequent products.
+    *
+    * */
+}
