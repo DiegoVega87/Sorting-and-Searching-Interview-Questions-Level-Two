b34c3d68d11d77e4d9937710d5cc9366464c3f5c
Diego Vega <diegovega@me.com>
2023-08-08 17:38:35 +0200

    Add examples

--- src/MostSoldProducts.java
+++ src/MostSoldProducts.java
@@ -5,5 +5,20 @@ public class MostSoldProducts {
     * We need you to write a function that takes in an integer array of sold product IDs and an integer k, and returns
     * the k most frequent products.
     *
+    * Example 1:
+    * int[] products = {1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 1};
+    * int k = 2;
+    * List<Integer> result = topKFrequent(products, k);
+    * System.out.println(result);
+    * Output:[1, 2
+    *
+    * Example 2:
+    * int[] products = {1, 2, 3, 4, 5, 1, 2, 3, 1, 2, 1};
+    * int k = 3;
+    * List<Integer> result = topKFrequent(products, k);
+    * System.out.println(result);
+    * Output:[1, 2, 3]
+    *
+    *
     * */
 }
