0c960ae04785a159d72d444fcb087b19fc69ab4a
Diego Vega <diegovega@me.com>
2023-08-09 15:42:16 +0200

    Add problem explanation

---
+++ src/FindingBestSalesDays.java
@@ -0,0 +1,12 @@
+public class FindingBestSalesDays {
+
+    /*
+    * As a data analyst at a fast-growing startup, you have been tasked with identifying the peak sales periods for
+    * the company. You have been provided with an integer array 'sales_data' which represents the daily sales figures
+    * for the past month. Your task is to write a function that can find a peak element in the 'sales_data' array.
+    * A peak element is defined as an element that is strictly greater than its neighbors, thus a better result was
+    * achieved for this day. Identify the indices of all peak elements in sales_data.
+    *
+    *
+    * */
+}
