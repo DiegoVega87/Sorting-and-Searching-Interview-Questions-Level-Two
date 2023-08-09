7fd9c5af5db65e533ba21b5ef5e963689b4b8b2b
Diego Vega <diegovega@me.com>
2023-08-09 12:51:06 +0200

    Add proble explanation

---
+++ src/SpecificComplaints.java
@@ -0,0 +1,10 @@
+public class SpecificComplaints {
+
+    /*
+    * We have a large database of customer feedback, and we want to analyze the most common complaints. We need a
+    * function that takes an integer array representing how often each unique complaint was made and an integer k,
+    * the rank of the element we want to find in the array, and returns the complaint with the k largest frequency.
+    *
+    * */
+
+}
