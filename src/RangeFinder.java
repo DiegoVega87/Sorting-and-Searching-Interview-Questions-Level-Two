4625fe05a3533b3af065154b1102a1f4c58365bd
Diego Vega <diegovega@me.com>
2023-08-09 16:07:28 +0200

    Add proble explanation

---
+++ src/RangeFinder.java
@@ -0,0 +1,10 @@
+public class RangeFinder {
+
+    /*
+    * Your startup has a database of customer purchase histories. You need to create a function that takes an array
+    * of integers representing the purchase amounts sorted in non-decreasing order and an integer representing the exact
+    * amount sold. The function should return an array of two integers representing the starting and ending positions
+    * of the target purchase amount.
+    *
+    * */
+}
