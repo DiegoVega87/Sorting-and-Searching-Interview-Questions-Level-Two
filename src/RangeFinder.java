eab4785526fd51cbe70ea53e92cd79e7de09fe0f
Diego Vega <diegovega@me.com>
2023-08-09 16:08:25 +0200

    Add examples

--- src/RangeFinder.java
+++ src/RangeFinder.java
@@ -6,5 +6,16 @@ public class RangeFinder {
     * amount sold. The function should return an array of two integers representing the starting and ending positions
     * of the target purchase amount.
     *
+    * Example 1:
+    * int[] purchases = {10, 20, 35, 40, 50};
+    * int targetAmount = 30;
+    * int[] expectedOutput = {0, 1};
+    *
+    * Example 2:
+    * int[] purchases = {5, 10, 15, 22, 25, 30, 35};
+    * int targetAmount = 25;
+    * int[] expectedOutput = {1, 2};
+    *
+    *
     * */
 }
