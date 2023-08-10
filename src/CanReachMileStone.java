c6d0f2b38ee806934a19047e5dada5b977cfdd30
Diego Vega <diegovega@me.com>
2023-08-10 10:53:06 +0200

    Add problem explanation

---
+++ src/CanReachMileStone.java
@@ -0,0 +1,13 @@
+public class CanReachMileStone {
+
+    /*
+    * As a startup, you are given a series of milestones that you need to achieve in order to reach your final goal.
+    * Each milestone has a specific value representing the maximum progress you can make from that point. Your
+    * task is to create a function that takes in an integer array representing these values and returns true if you can
+    * reach the final milestone, or false otherwise. The function should start from the first milestone and iterate
+    * through each element in the array, checking if it's possible to reach the next milestone. If you can't reach a
+    * milestone with any combination of progress values, return false.
+    *
+    *
+    * */
+}
